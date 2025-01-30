b778c001fc90576c81ee626b33c20558ef0c1da8
Denis-creator-marker <dengoog174@gmail.com>
2025-01-30 19:49:55 +0300

    add double jump for player

--- Essentials Project/Assets/_Unity Essentials/Denis52/Scripts/PlayerController.cs
+++ Essentials Project/Assets/_Unity Essentials/Denis52/Scripts/PlayerController.cs
@@ -4,8 +4,11 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float _speed = 5.0f;
     [SerializeField] private float _rotationSpeed = 120f;
+    [SerializeField] private float jumpForce = 5.0f;
 
     private Rigidbody _rigidbody;
+    private int _jumpCount  = 0;
+    private int _maxJumps = 2;
 
     private void Start()
     {
@@ -14,12 +17,31 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        Jump();
+        MovePlayer();
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && _jumpCount < _maxJumps)
+        {
+            _jumpCount++;
+            _rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
+        }
+    }
+
+    private void MovePlayer()
+    {
         float moveVertical = Input.GetAxis("Vertical");
         Vector3 movement = transform.forward * moveVertical * _speed * Time.fixedDeltaTime;
-        _rigidbody.MovePosition(_rigidbody.position +  movement);
+        _rigidbody.MovePosition(_rigidbody.position + movement);
 
         float turn = Input.GetAxis("Horizontal") * _rotationSpeed * Time.fixedDeltaTime;
         Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
         _rigidbody.MoveRotation(_rigidbody.rotation * turnRotation);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        _jumpCount = 0;
     }
 }
